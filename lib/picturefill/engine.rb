0d82a04006072ce019dad71b1db9255b6e379ca2
Kristian Mandrup <kmandrup@gmail.com>
2012-09-04 14:27:37 +0200

    fix engine

--- lib/picturefill/engine.rb
+++ lib/picturefill/engine.rb
@@ -1,8 +1,8 @@
 module Picturefill
   module Rails
     class Engine < ::Rails::Engine
-      initializer ' picturefill for rails setup' do
-        ActiveView::Base.send :include, Picturefill::ViewHelper
+      initializer 'picturefill for rails' do
+        ActionView::Base.send :include, ::Picturefill::ViewHelper
       end
     end
   end
